b1b6c44bdc56eac81db63d5bb55f48d2701c41ab
Ken Cunningham <kencu@macports.org>
2023-10-08 09:55:35 -0700

    pan: update to 0.154

--- pan.rb
+++ pan.rb
@@ -1,10 +1,12 @@
 class Pan < Formula
   desc "Usenet newsreader that's good at both text and binaries"
   homepage "https://pan.rebelbase.com"
-  url "https://pan.rebelbase.com/download/releases/0.149/source/pan-0.149.tar.bz2"
-  sha256 "5d34d0d7dbb73ee53484ffe173f01d8b2c0732126ec21d8cf29d6baa5ecb85ea"
+  url "https://gitlab.gnome.org/GNOME/pan/-/archive/v0.154/pan-v0.154.tar.bz2"
+  sha256 "440317954df7217689100df3dfb68865770f5aed1b8ed2b45432d771bb80a8c9"
   license "GPL-2.0-only"
 
+  depends_on "automake" => :build
+  depends_on "autoconf" => :build
   depends_on "itstool" => :build
   depends_on "pkg-config" => :build
   depends_on "adwaita-icon-theme"
@@ -21,18 +23,19 @@ class Pan < Formula
   depends_on "pango"
 
   def install
-    # brew provides enchant-2, which is accepted for the pan build but needs include dir changed in source
-    inreplace  "pan/gui/group-prefs-dialog.cc", "<enchant/enchant.h>", "<enchant-2/enchant.h>"
 
     # use brew name for gtk3 version of tool update-icon-cache
-    inreplace  "pan/icons/Makefile.in", "gtk-update-icon-cache", "gtk3-update-icon-cache"
+    inreplace  "pan/icons/Makefile.am", "gtk-update-icon-cache", "gtk3-update-icon-cache"
 
     ENV.append "LDFLAGS", "-liconv"
+    ENV.append "CXXFLAGS", "-std=c++11"
 
+    # there is a better way to do this rather than hardcode it, I'm certain
+    ENV.append "CPPFLAGS", "-I/opt/homebrew/Cellar/gnutls/3.8.1/include -I/opt/homebrew/Cellar/enchant/2.6.1/include"
+
+    system "./autogen.sh"
     system "./configure", *std_configure_args,
                           "--disable-silent-rules",
-                          "--with-gmime30",
-                          "--with-gtk3",
                           "--with-gnutls"
     system "make"
     system "make", "install"
