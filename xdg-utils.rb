84aae8314c6112160b5adfea567ae5e69f5b7276
Ken Cunningham <kencu@macports.org>
2023-10-09 18:59:25 -0700

    xdg-utils: fix audit, fix test

--- xdg-utils.rb
+++ xdg-utils.rb
@@ -1,17 +1,16 @@
 class XdgUtils < Formula
-  desc "A set of simple scripts that provide basic desktop integration functions for any Free Desktop, such as Linux."
+  desc "Scripts that provide basic desktop integration functions"
   homepage "https://gitlab.freedesktop.org/xdg/xdg-utils"
   url "https://gitlab.freedesktop.org/xdg/xdg-utils/-/archive/v1.1.3/xdg-utils-v1.1.3.tar.gz"
   sha256 "7dfc542f9bff8ccd673a4f45c671f8f6bf4d1f191cff017f68e94a27182af01f"
   license :public_domain
 
-  depends_on "xmlto" => :build
   depends_on "docbook" => :build
   depends_on "docbook-xsl" => :build
   depends_on "lynx" => :build
+  depends_on "xmlto" => :build
 
   def install
-
     ENV.append "XML_CATALOG_FILES", "/opt/homebrew/etc/xml/catalog"
 
     system "./configure", *std_configure_args
@@ -20,15 +19,6 @@ class XdgUtils < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test xdg-utils`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    assert_match "xdg-open" + " " + version.to_s, shell_output("#{bin}/xdg-open --version")
   end
 end
